af09f4df6768dfa91d1b776924d0a85b3308b53c
LaMavia <ts438730@students.mimuw.edu.pl>
2023-11-27 21:01:02 +0100

    update id literal to a config constant (#1)
    Signed-off-by: LaMavia <ts438730@students.mimuw.edu.pl>

--- scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/flow/open/ScalaBspProjectOpenProcessor.scala
+++ scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/flow/open/ScalaBspProjectOpenProcessor.scala
@@ -2,10 +2,11 @@ package org.jetbrains.plugins.scala.bsp.flow.open
 
 import org.jetbrains.plugins.bsp.flow.open.{BspProjectOpenProcessorExtension, BuildToolId}
 import org.jetbrains.plugins.scala.bsp.BspFeatureFlags
+import org.jetbrains.plugins.scala.bsp.config.ScalaPluginConstants
 
 class ScalaBspProjectOpenProcessor extends BspProjectOpenProcessorExtension {
 
   override def getShouldBspProjectOpenProcessorBeAvailable: Boolean = BspFeatureFlags.isBspPluginIntegrationEnabled
 
-  override def getBuildToolId: BuildToolId = new BuildToolId("scalabsp")
+  override def getBuildToolId: BuildToolId = new BuildToolId(ScalaPluginConstants.SYSTEM_ID.getId)
 }
