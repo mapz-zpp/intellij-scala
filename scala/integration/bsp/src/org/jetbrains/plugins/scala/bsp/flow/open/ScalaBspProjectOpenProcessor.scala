2dc68fccfae43ac5d60c7c3a64df7e88e14055c6
Zuzanna Surowiec <ts438730@students.mimuw.edu.pl>
2023-12-01 13:31:18 +0100

    Update ScalaBspProjectOpenProcessor.scala
    remove legacy import

--- scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/flow/open/ScalaBspProjectOpenProcessor.scala
+++ scala/integration/bsp/src/org/jetbrains/plugins/scala/bsp/flow/open/ScalaBspProjectOpenProcessor.scala
@@ -2,7 +2,6 @@ package org.jetbrains.plugins.scala.bsp.flow.open
 
 import org.jetbrains.plugins.bsp.flow.open.{BspProjectOpenProcessorExtension, BuildToolId}
 import org.jetbrains.plugins.scala.bsp.BspFeatureFlags
-import org.jetbrains.plugins.scala.bsp.config.ScalaPluginConstants
 
 class ScalaBspProjectOpenProcessor extends BspProjectOpenProcessorExtension {
 
