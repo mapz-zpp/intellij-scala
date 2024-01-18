5238b259dcb4d75d0e846a33d46f666bd8461481
mikolaj <mikolajtuchola@gmail.com>
2024-01-18 15:01:27 +0100

    Hide root directory in module #2

--- scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/extension/ScalaBuildTargetClassifier.scala
+++ scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/extension/ScalaBuildTargetClassifier.scala
@@ -1,11 +1,9 @@
 package org.jetbrains.plugins.scala.bsp.extension
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.{Project, ProjectManager}
 import com.intellij.openapi.roots.ProjectRootManager
-import com.intellij.serviceContainer.ComponentManagerImpl
 import org.jetbrains.plugins.bsp.extension.points.{BuildTargetClassifierExtension, BuildToolId}
 import org.jetbrains.plugins.scala.bsp.config.ScalaPluginConstants
 import org.jetbrains.plugins.scala.bsp.server.connection.ScalaConnectionDetailsProviderService
@@ -17,13 +15,29 @@ private class ScalaBuildTargetClassifier extends BuildTargetClassifierExtension 
   private val logger = Logger.getInstance(this.getClass)
 
   private val buildTargetFileRegex = """(file:/)(.*)/(#[^/]*/[^/]*)""".r
+  private val buildTargetHiddenFileRegex = """(file:/+)([^/]+/.*)""".r
   override val getBuildToolId: BuildToolId = ScalaPluginConstants.BUILD_TOOL_ID
   override val getSeparator = "/"
 
   override def calculateBuildTargetPath(buildTarget: String): util.List[String] = {
     buildTarget match {
       case buildTargetFileRegex(_, path, _) =>
-        path.split("/").toList.takeRight(2).asJava
+        val projects = ProjectManager.getInstance().getOpenProjects.filter(p => p.getBasePath.contains(path))
+        if (projects.length > 1) {
+          logger.warn(s"buildTargetClassifier found more than one project with provided path ($path)")
+          path.split("/").toList.asJava
+        }
+        val tmp = projects.head.getActualComponentManager.getService(classOf[ScalaConnectionDetailsProviderService]).getState.projectRootPath
+        if (tmp.isEmpty){
+          path.split("/").toList.asJava
+        }
+
+        tmp.get.getParent.toString match {
+          case buildTargetHiddenFileRegex(_, toHidePath) =>
+            path.replace(toHidePath, "").stripPrefix("/").split("/").toList.asJava
+          case _ =>
+            List.empty.asJava
+        }
       case _ =>
         logger.warn(s"buildTargetFileRegex mismatch, s=$buildTarget")
         List.empty.asJava
