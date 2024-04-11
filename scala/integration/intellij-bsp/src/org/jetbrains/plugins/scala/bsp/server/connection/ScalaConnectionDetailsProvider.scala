6c6f7fe1eb17550ff43cfd64270a472ad0bf5e2b
Przemysław Fuchs <fuchs.przemyslaw@gmail.com>
2024-04-11 12:12:49 +0200

    Implement persistence for BSP storage (#32)
    * Implement persistence for BSP storage
    
    * Add newline at the end of file

--- scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/server/connection/ScalaConnectionDetailsProvider.scala
+++ scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/server/connection/ScalaConnectionDetailsProvider.scala
@@ -2,96 +2,21 @@ package org.jetbrains.plugins.scala.bsp.server.connection
 
 import ch.epfl.scala.bsp4j.BspConnectionDetails
 import com.intellij.modcommand.ModCommand.error
-import com.intellij.openapi.components.{PersistentStateComponent, Service, State, Storage, StoragePathMacros}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.JavaCoroutines
-import kotlin.coroutines.Continuation
 import org.jetbrains.bsp.protocol.utils.ParsersKt
 import org.jetbrains.plugins.bsp.extension.points.BuildToolId
-import org.jetbrains.plugins.bsp.server.connection.ConnectionDetailsProviderExtension
+import org.jetbrains.plugins.bsp.server.connection.ConnectionDetailsProviderExtensionJavaShim
+import org.jetbrains.plugins.scala.bsp.ScalaBspMetadataStorage
 import org.jetbrains.plugins.scala.bsp.config.ScalaPluginConstants.BUILD_TOOL_ID
 
 import java.lang
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{CompletableFuture, TimeUnit}
 import scala.jdk.CollectionConverters._
 
-class ScalaConnectionDetailsProvider extends ConnectionDetailsProviderExtension {
-  override def onFirstOpening(project: Project, projectPath: VirtualFile, continuation: Continuation[_ >: lang.Boolean]): AnyRef = {
-    //noinspection ApiStatus,UnstableApiUsage
-    JavaCoroutines.suspendJava[lang.Boolean](cont => {
-      saveProjectRootFile(project, projectPath)
-      val connectionFile = getConnectionFile(projectPath)
-      if (connectionFile.isDefined) {
-        // connection file already exists - exit
-        cont.resume(true)
-      } else {
-        // generate our own connection file
-        val hasGeneratedFile = generateSbtConnectionFile(projectPath).isDefined
-        cont.resume(hasGeneratedFile)
-      }
-    }, continuation)
-  }
-
-  override def provideNewConnectionDetails(project: Project, bspConnectionDetails: BspConnectionDetails): BspConnectionDetails = {
-    if (bspConnectionDetails != null) {
-      return bspConnectionDetails
-    }
-
-    val projectRootPath = getProjectRootFile(project) match {
-      case Some(projectFile) => projectFile
-      case None =>
-        error("Cannot obtain project path, please reimport the project.")
-        return bspConnectionDetails
-    }
-
-    val connectionFile = getConnectionFile(projectRootPath) match {
-      case None => generateSbtConnectionFile(projectRootPath)
-      case other => other
-    }
-
-    connectionFile match {
-      case None => null
-      case Some(connectionFile) => parseConnectionFile(connectionFile).orNull
-    }
-  }
-
-
-  override def getBuildToolId: BuildToolId = BUILD_TOOL_ID
-
-  private def parseConnectionFile(connectionFile: VirtualFile): Option[BspConnectionDetails] =
-    Option(ParsersKt.parseBspConnectionDetails(connectionFile))
-
-  private def saveProjectRootFile(project: Project, projectRootFile: VirtualFile): Unit =
-    project.getActualComponentManager.getService(classOf[ScalaConnectionDetailsProviderService])
-      .loadState(new ScalaConnectionDetailsProviderMetadata(Option(projectRootFile)))
-
-  private def getProjectRootFile(project: Project): Option[VirtualFile] = {
-    project.getActualComponentManager.getService(classOf[ScalaConnectionDetailsProviderService])
-      .getState
-      .projectRootPath
-  }
-
+class ScalaConnectionDetailsProvider extends ConnectionDetailsProviderExtensionJavaShim {
   private def getConnectionFile(projectPath: VirtualFile): Option[VirtualFile] = {
     getChild(projectPath, List.apply(".bsp", "sbt.json"))
-  }
-
-  private def generateSbtConnectionFile(projectPath: VirtualFile): Option[VirtualFile] = {
-    val process = new ProcessBuilder("coursier", "launch", "sbt", "--", "bspConfig")
-      .directory(projectPath.toNioPath.toFile)
-      .start
-
-    if (process.waitFor(2, TimeUnit.MINUTES)) {
-      if (process.exitValue() != 0) {
-        val processInput = process.inputReader.lines.toList.asScala.mkString
-        val processError = process.errorReader.lines.toList.asScala.mkString
-        throw new RuntimeException(s"Sbt bsp file genration with coursier failed. stdout=[$processInput], stderr=[$processError]")
-      }
-
-      getConnectionFile(projectPath)
-    } else {
-      throw new RuntimeException("Sbt bsp file generation timed out")
-    }
   }
 
   private def getChild(root: VirtualFile, path: List[String]): Option[VirtualFile] = {
@@ -108,22 +33,61 @@ class ScalaConnectionDetailsProvider extends ConnectionDetailsProviderExtension 
       vf
     })
   }
-}
 
-class ScalaConnectionDetailsProviderMetadata(var projectRootPath: Option[VirtualFile])
+  private def generateConnectionFile(projectPath: VirtualFile): CompletableFuture[lang.Boolean] =
+    CompletableFuture.supplyAsync(() => {
+      val process = new ProcessBuilder("coursier", "launch", "sbt", "--", "bspConfig")
+        .directory(projectPath.toNioPath.toFile)
+        .start()
 
-@State(
-  name = "ScalaConnectionDetailsProviderService",
-  storages = Array(new Storage(StoragePathMacros.WORKSPACE_FILE)),
-  reportStatistic = true
-)
-@Service(Array(Service.Level.PROJECT))
-final class ScalaConnectionDetailsProviderService extends PersistentStateComponent[ScalaConnectionDetailsProviderMetadata] {
-  private var projectRootPath: Option[VirtualFile] = Option.empty
+      process.waitFor(2, TimeUnit.MINUTES)
+      if (process.exitValue() != 0) {
+        val processInput = process.inputReader.lines.toList.asScala.mkString
+        val processError = process.errorReader.lines.toList.asScala.mkString
+        throw new RuntimeException(s"Sbt bsp file genration with coursier failed. stdout=[$processInput], stderr=[$processError]")
+      }
 
-  override def getState: ScalaConnectionDetailsProviderMetadata =
-    new ScalaConnectionDetailsProviderMetadata(projectRootPath)
+      getConnectionFile(projectPath).isDefined
+    })
 
-  override def loadState(state: ScalaConnectionDetailsProviderMetadata): Unit =
-    this.projectRootPath = state.projectRootPath
+  override def onFirstOpening(project: Project, projectPath: VirtualFile): CompletableFuture[lang.Boolean] = {
+    val metadataStorage = ScalaBspMetadataStorage(project)
+
+    val metadataStorageState = metadataStorage.getState
+    metadataStorageState.saveProjectRootFile(projectPath)
+
+    metadataStorage.loadState(metadataStorageState)
+
+    getConnectionFile(projectPath) match {
+      case None => generateConnectionFile(projectPath)
+      case _ => CompletableFuture.completedFuture(true)
+    }
+  }
+
+  override def provideNewConnectionDetails(project: Project, currentDetails: BspConnectionDetails): BspConnectionDetails = {
+    val maybeProjectRootPath = ScalaBspMetadataStorage(project).getState.getProjectRootFile
+
+    val projectRootPath = maybeProjectRootPath match {
+      case Some(projectRootPath) => projectRootPath
+      case None =>
+        error("Cannot obtain project path, please reimport the project.")
+        return null
+    }
+
+    val connectionFile = getChild(projectRootPath, List.apply(".bsp", "sbt.json")) match {
+      case Some(connectionFile) => connectionFile
+      case None =>
+        error("BSP connection file doesn't exist, please reimport the project or generate the file manually.")
+        return null
+    }
+
+    val newDetails = ParsersKt.parseBspConnectionDetails(connectionFile)
+    if (newDetails != null && newDetails.equals(currentDetails)) {
+      null
+    } else {
+      newDetails
+    }
+  }
+
+  override def getBuildToolId: BuildToolId = BUILD_TOOL_ID
 }
