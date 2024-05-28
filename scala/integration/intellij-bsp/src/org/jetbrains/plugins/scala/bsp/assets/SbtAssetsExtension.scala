971eadcffd41c45ca070372a42408b876efee91b
mikolaj <mikolajtuchola@gmail.com>
2024-05-28 20:52:15 +0200

    Changed SbtAssetsExtension to the new interface.

--- scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/assets/SbtAssetsExtension.scala
+++ scala/integration/intellij-bsp/src/org/jetbrains/plugins/scala/bsp/assets/SbtAssetsExtension.scala
@@ -10,13 +10,11 @@ import javax.swing.Icon
 class SbtAssetsExtension extends BuildToolAssetsExtension {
   override def getIcon: Icon = IconLoader.getIcon("icons/buildServerProtocol.svg", classOf[SbtAssetsExtension])
 
-  override def getLoadedTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget.svg", classOf[SbtAssetsExtension])
-
   override def getInvalidTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget_red.svg", classOf[SbtAssetsExtension])
-
-  override def getUnloadedTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget_grey.svg", classOf[SbtAssetsExtension])
 
   override def getPresentableName: String = "sbt-bsp"
 
   override def getBuildToolId: BuildToolId = ScalaPluginConstants.BUILD_TOOL_ID
+
+  override def getTargetIcon: Icon = IconLoader.getIcon("icons/buildServerProtocolTarget.svg", classOf[SbtAssetsExtension])
 }
